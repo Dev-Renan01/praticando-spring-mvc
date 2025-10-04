52d94f326a63cb85273e646ebeb4aa88a5ea4265
Thiago Renan <thiagorenan1108@gmail.com>
2025-10-04 16:31:59 -0300

    Testando a aplicação

--- spring-boot-mvc/src/main/java/com/spring_boot_mvc/controller/OlaController.java
+++ spring-boot-mvc/src/main/java/com/spring_boot_mvc/controller/OlaController.java
@@ -1,15 +1,12 @@
 package com.spring_boot_mvc.controller;
-
-import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class OlaController {
 
-    @RequestMapping (value = "/boasvindas/{nome}", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public String ola(@PathVariable String nome){
-
-        return "Ola, " + nome + "seja bem vindo ao meu projeto!";
+    @RequestMapping (value = "/")
+    public String index(){
+        return "index";
     }
 }
