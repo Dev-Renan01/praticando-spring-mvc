ca09c53c79014f601df66338b2fabf302b7d41d3
Thiago Renan <thiagorenan1108@gmail.com>
2025-10-04 22:16:07 -0300

    Listar Pessoas

--- spring-boot-mvc/src/main/java/com/spring_boot_mvc/controller/PessoaController.java
+++ spring-boot-mvc/src/main/java/com/spring_boot_mvc/controller/PessoaController.java
@@ -4,6 +4,9 @@ import com.spring_boot_mvc.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Iterator;
 
 @Controller
 public class PessoaController {
@@ -14,7 +17,6 @@ public class PessoaController {
 
     @RequestMapping (value = "/cadastropessoa", method = RequestMethod.GET)
     public String inicio(){
-
         return "cadastro/cadastropessoa";
     }
 
@@ -23,6 +25,17 @@ public class PessoaController {
 
         pessoaRepository.save(pessoa);
         return "cadastro/poscadastro";
+    }
+
+    @RequestMapping(value = "/listarpessoas", method = RequestMethod.GET)
+    public ModelAndView pessoas(){
+
+        ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+        Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+
+        modelAndView.addObject("pessoas", pessoasIt);
+
+        return modelAndView;
     }
 
 }
