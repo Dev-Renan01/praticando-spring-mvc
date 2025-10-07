11a1b628e41962ab33b92f7be783d1779510e23e
Thiago Renan <thiagorenan1108@gmail.com>
2025-10-07 19:02:22 -0300

    Editando cadastro com thymeleaf e o Spring

--- spring-boot-mvc/src/main/java/com/spring_boot_mvc/controller/PessoaController.java
+++ spring-boot-mvc/src/main/java/com/spring_boot_mvc/controller/PessoaController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Iterator;
+import java.util.Optional;
 
 @Controller
 public class PessoaController {
@@ -16,15 +17,24 @@ public class PessoaController {
 
 
     @RequestMapping (value = "/cadastropessoa", method = RequestMethod.GET)
-    public String inicio(){
-        return "cadastro/cadastropessoa";
+    public ModelAndView inicio(){
+
+        ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+        andView.addObject("pessoaobj", new Pessoa());
+        return andView;
     }
 
-    @RequestMapping(value = "/salvarpessoa", method = RequestMethod.POST)
-    public String salvar(Pessoa pessoa){
-
+    @RequestMapping(value = "/{path:.*}/salvarpessoa", method = RequestMethod.POST)
+    public ModelAndView salvar(Pessoa pessoa){
         pessoaRepository.save(pessoa);
-        return "cadastro/poscadastro";
+
+        ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+        Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+
+        modelAndView.addObject("pessoas", pessoasIt);
+        modelAndView.addObject("pessoaobj", new Pessoa());
+
+        return modelAndView;
     }
 
     @RequestMapping(value = "/listarpessoas", method = RequestMethod.GET)
@@ -34,8 +44,19 @@ public class PessoaController {
         Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
 
         modelAndView.addObject("pessoas", pessoasIt);
+        modelAndView.addObject("pessoaobj", new Pessoa());
 
         return modelAndView;
+    }
+
+    @GetMapping(value = "/editarpessoa/{idpessoa}")
+    public ModelAndView editar(@PathVariable("idpessoa") Long idpessoa){
+
+        Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa);
+
+        ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+        andView.addObject("pessoaobj", pessoa.get());
+        return andView;
     }
 
 }
